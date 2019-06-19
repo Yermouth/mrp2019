e4a787bed49e44cc62ad7c539869b22dca3ffd6c
Yermouth <sunsunlai826@yahoo.com.hk>
2019-06-20 00:44:53 +0800

    Add ipynb for data exploration and plotting

---
+++ download.sh
@@ -0,0 +1,7 @@
+wget http://svn.nlpl.eu/mrp/2019/public/sample.tgz
+tar zxvf sample.tgz
+
+wget http://svn.nlpl.eu/mrp/2019/public/companion.tgz
+tar zxvf companion.tgz
+
+git clone https://github.com/cfmrp/mtool
